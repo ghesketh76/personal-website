00b3e7467b0dcb37a513d1d1af3e8e685ed5dc18
ghesketh76 <ghesketh76@gmail.com>
2021-04-21 16:20:03 -0600

    added interest list

--- src/pages/Interests.js
+++ src/pages/Interests.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Interests() {
     return (
         <div>
-            
+            <ul>
+                <li>This is an interest</li>
+                <li>This is an interest</li>
+                <li>This is an interest</li>
+            </ul>
         </div>
     )
 }
