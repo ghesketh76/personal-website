2065d74913c6107a394a1eef4c8e65df3bec2655
ghesketh76 <ghesketh76@gmail.com>
2021-04-22 09:34:53 -0600

    added contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Contact() {
+    return (
+        <div>
+            <ul>
+                <li>Linked In</li>
+                <li>GitHub</li>
+                <li>Email</li>
+            </ul>
+        </div>
+    )
+}
