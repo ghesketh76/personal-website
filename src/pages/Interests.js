db3d784f63b7bf994bcf1423016ca3e96ad8a276
ghesketh76 <ghesketh76@gmail.com>
2021-04-26 09:54:54 -0600

    tweaks

--- src/pages/Interests.js
+++ src/pages/Interests.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Interests() {
     return (
         <div>
+            <h2>this is a page where i talk about my interests</h2>
             <ul>
                 <li>This is an interest</li>
                 <li>This is an interest</li>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Portfolio() {
     return (
         <div>
+            <h2>I will give a description of my coding expereince and a primer to my projects</h2>
             <ul>
                 <li>project 1</li>
                 <li>project 2</li>
