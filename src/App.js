f253d49eb3871c50734827ef7052ac84e48c00b5
ghesketh76 <ghesketh76@gmail.com>
2021-04-30 11:45:42 -0600

    header tabs

--- src/App.js
+++ src/App.js
@@ -10,7 +10,14 @@ function App() {
   
   return (
     <div className="App">
-      <header>Up here will be some buttons to click to take you to various parts of the site</header>
+      <header>
+        <h1> Up here will be some buttons to click to take you to various parts of the site</h1>
+        <ul>
+          <li>Tab 1</li>
+          <li>Tab 2</li>
+          <li>Tab 3</li>
+        </ul>
+        </header>
       <h1>This is my website</h1>
       <h2>This is where i will put a blurb about myself</h2>
       <h3>put a picture of myself here</h3>
