1f1239fe8d0207a4af40a9f8d109e7682b74850a
ghesketh76 <ghesketh76@gmail.com>
2021-05-03 13:14:36 -0600

    mission statement

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         </ul>
         </header>
       <h1>This is my website</h1>
-      <h2>This is where i will put a blurb about myself</h2>
+      <h2>Experienced in Ruby on Rails and JavaScript based programming and a background in Oil and Gas Engineering. Possess strong skills in project management, problem solving, and leadership from the fast paced oil and gas industry that would be a valuable asset to technology companies.</h2>
       <h3>put a picture of myself here</h3>
       <p>Some picyures of what i like to do</p>
       <p>This will be a paragraph about myself</p>
