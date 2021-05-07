37998998d436c9f10ad97470be5abb0b8e01168f
ghesketh76 <ghesketh76@gmail.com>
2021-05-07 10:34:11 -0600

    footer shit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,8 @@ export default function Footer() {
     return (
         <div>
             <h2>Heres some footer bullshit</h2>
+            <p>Small print stuff</p>
+            <p>Maybe do some funny shit</p>
         </div>
     )
 }
