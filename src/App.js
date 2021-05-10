46afecb8a6568b9ffbdf373d2acd69b7c3f86e08
ghesketh76 <ghesketh76@gmail.com>
2021-05-10 11:39:45 -0600

    header component

--- src/App.js
+++ src/App.js
@@ -10,14 +10,6 @@ function App() {
   
   return (
     <div className="App">
-      <header>
-        <h1> Up here will be some buttons to click to take you to various parts of the site</h1>
-        <ul>
-          <li>Tab 1</li>
-          <li>Tab 2</li>
-          <li>Tab 3</li>
-        </ul>
-        </header>
       <h1>This is my website</h1>
       <h2>Experienced in Ruby on Rails and JavaScript based programming and a background in Oil and Gas Engineering. Possess strong skills in project management, problem solving, and leadership from the fast paced oil and gas industry that would be a valuable asset to technology companies.</h2>
       <h3>put a picture of myself here</h3>
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <h1> Up here will be some buttons to click to take you to various parts of the site</h1>
+            <ul>
+                <li>Tab 1</li>
+                <li>Tab 2</li>
+                <li>Tab 3</li>
+            </ul>
+        </header>
+    )
+}
