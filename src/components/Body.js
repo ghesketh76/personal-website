16eb822c47faeea37ad6497678370445cee64903
ghesketh76 <ghesketh76@gmail.com>
2021-05-11 10:21:45 -0600

    body copmponent

---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Body() {
+    return (
+        <div>
+            <h1>This is my website</h1>
+            <h2>Experienced in Ruby on Rails and JavaScript based programming and a background in Oil and Gas Engineering. Possess strong skills in project management, problem solving, and leadership from the fast paced oil and gas industry that would be a valuable asset to technology companies.</h2>
+            <h3>put a picture of myself here</h3>
+            <p>Some picyures of what i like to do</p>
+            <p>This will be a paragraph about myself</p>
+        </div>
+    )
+}
