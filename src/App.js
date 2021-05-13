d74b7e69f160f573a575c548c7a2be454ea99a62
ghesketh76 <ghesketh76@gmail.com>
2021-05-13 10:56:57 -0600

    components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@
 import './App.css';
 import {data} from '../src/components/data'
 import Header from './components/Header';
+import Footer from './components/footer';
+import Body from './components/Body';
 
 function App() {
 
   const name = data.name
-  const fullNaame = data.fullName
+  const fullName = data.fullName
   const occupation = data.occupation
   
   return (
     <div className="App">
-      <Header />
-  
+      <Header name={name} fullName={fullName} occupation={occupation}/>
+      <Body name={name} fullName={fullName} occupation={occupation}/>
+      <Footer name={name} fullName={fullName} occupation={occupation}/>
     </div>
   );
 }
