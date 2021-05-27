2f6e5fd7f3adb7489eb99e7f3afcfb10b6ebfa26
ghesketh76 <ghesketh76@gmail.com>
2021-05-27 11:56:40 -0600

    links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,10 +4,9 @@ export default function Contact() {
     return (
         <div>
             <ul>
-                <li>Linked In</li>
-                    <a href="https://www.linkedin.com/in/granthesketh/">LinkedIn</a>
-                <li>GitHub</li>
-                <li>Email</li>
+                <li><a href="https://www.linkedin.com/in/granthesketh/">LinkedIn</a></li>       
+                <li><a href="https://github.com/ghesketh76">Github</a></li>       
+                <li><a href="ghesketh76@gmail.com">Gmail</a></li>
             </ul>
         </div>
     )
